fb213b8eb9f8c6a6194308dfd6381c3fe4b59cd9
Jack Yeh <jack.yeh@gmail.com>
2015-09-18 16:52:39 -0600

    add functionality: specifying custom serializers - serializers default to the serializer associated with that controller   + example: Api::V1::ItemsController defaults to app/seralizers/item_serializer.rb
    - by specifying a customer serializer, each action within the controller can have its own serializer
    - they do not all have to use the default item_serializer

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ItemsController < ApplicationController
   respond_to :json
 
   def index
-    respond_with Item.all
+    respond_with Item.all, serializer: JonSnowSerializer
   end
 
   def show
