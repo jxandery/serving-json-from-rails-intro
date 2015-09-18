fbe219d90269418bcf29ee30860744aac40afc2b
Jack Yeh <jack.yeh@gmail.com>
2015-09-18 16:58:22 -0600

    add functionality: you can create custom methods on the serializer and have it displayed - suppose you want the response to also return the order count:   + you can create a custom method order_count in the serializer class   + object is a lot like self, but in this case object is the object that is being serialized   + example: items#index each item returned in the reponse is an item object and each of these ojbects will return the number of orders
    * pretty cool stuff

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -2,8 +2,12 @@ class ItemSerializer < ActiveModel::Serializer
   attributes :id,
              :name,
              :description,
-             :image_url
+             :image_url,
+             :order_count
 
   has_many :orders
 
+  def order_count
+    object.orders.count
+  end
 end
