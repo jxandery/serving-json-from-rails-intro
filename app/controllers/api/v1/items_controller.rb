6fed87aea20a9f094b4d23c9ad1059afc763cf24
Jack Yeh <jack.yeh@gmail.com>
2015-09-18 16:54:59 -0600

    removing example of custom serializer from previous commit. it was used for demonstration purposes only

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ItemsController < ApplicationController
   respond_to :json
 
   def index
-    respond_with Item.all, serializer: JonSnowSerializer
+    respond_with Item.all
   end
 
   def show
